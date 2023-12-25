66b990b4e2ee04288daece9b772d4fd4fd08c1f2
Musa Samet Şen <96656977+msametsen@users.noreply.github.com>
2023-12-25 13:58:23 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // case sensitive bir dildir.Yani büyük küçük harfe duyarlıdır.Reusability tekrar tekrar kullanılabilirlik demek-Bir kere yaz her zaman kullan
+        int ogrenciSayisi = 12;
+        String mesaj ="Öğrenci Sayısı :";
+        System.out.println(mesaj + ogrenciSayisi);
+
+    }
+}
